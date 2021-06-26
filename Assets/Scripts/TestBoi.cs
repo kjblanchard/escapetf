8e9b586fcaf743ae6da89ba4e9902a5b7eea4e6c
Kevin Blanchard <blanchardkevinj@gmail.com>
2021-06-26 14:39:13 -0400

    Initial commit after creating unity proj

---
+++ Assets/Scripts/TestBoi.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TestBoi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Going to the store prolly");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
